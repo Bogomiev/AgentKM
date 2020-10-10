4edc02b0bdf4dbbec1499b3db9c7119618cd48d5
bogomiev <bogomiev@gmail.com>
2020-10-10 10:03:46 +1000

    Удалены лишние приложения

--- Agent_mobile/Agent_mobile/settings.py
+++ Agent_mobile/Agent_mobile/settings.py
@@ -33,8 +33,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap_datepicker_plus',
-    'mptt'
 ]
 
 MIDDLEWARE = [
