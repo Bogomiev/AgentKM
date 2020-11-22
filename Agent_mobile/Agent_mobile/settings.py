5d61829ed4e7276fb329aeb8854c68f13181157b
bogomiev <bogomiev@gmail.com>
2020-11-22 16:51:39 +1000

    1. Добавлена аутентификация через token (djoser). 2. Добавлена API для GET/POST - Agent

--- Agent_mobile/Agent_mobile/settings.py
+++ Agent_mobile/Agent_mobile/settings.py
@@ -1,4 +1,6 @@
+import datetime
 import os
+
 from .local_settings import set_settings
 from django.core.exceptions import ImproperlyConfigured
 
@@ -33,6 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'rest_framework_jwt',
+    'rest_framework_json_api'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +128,44 @@ if DEBUG:
     )
 else:
     STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAdminUser',
+        'rest_framework.permissions.AllowAny',
+    ),
+    'PAGE_SIZE': 1000,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework_json_api.pagination.PageNumberPagination',
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DATETIME_FORMAT': "%d.%m.%Y %H:%M:%S",
+}
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),  # default
+    # 'JWT_AUTH_HEADER_PREFIX': 'Bearer'
+}
+
+DJOSER = {
+    'SEND_ACTIVATION_EMAIL': False,
+    # 'SEND_CONFIRMATION_EMAIL': True,
+    'ACTIVATION_URL': 'auth/activate/{uid}/{token}/',
+    'PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND': True,
+    'PASSWORD_RESET_CONFIRM_URL': 'auth/reset/confirm/{uid}/{token}/',
+    'TOKEN_MODEL': None
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=2),
+}
--- Agent_mobile/Agent_mobile/urls.py
+++ Agent_mobile/Agent_mobile/urls.py
@@ -1,26 +1,18 @@
-"""Agent_mobile URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from .api.agent_views import AgentAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('Agent_mobile.apps.main_menu.urls')),
     path('index/', include('Agent_mobile.apps.main_menu.urls')),
     path('visit/', include('Agent_mobile.apps.visits.urls')),
     path('invoice/', include('Agent_mobile.apps.invoices.urls')),
     path('items/', include('Agent_mobile.apps.core.urls')),
+    url(r'^api/agent$', AgentAPI.as_view()),
+    url(r'^api/agent/(?P<guid>.+)$', AgentAPI.as_view()),
 ]
